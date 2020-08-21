042618cc9d5f2d819e5e386dc6b5bd567d515d30
Luke Monington <lukemonington3@gmail.com>
2020-08-21 03:44:04 -0600

    Added main data from competition

--- fraud_detection_main_ai.py
+++ fraud_detection_main_ai.py
@@ -1,2 +1,9 @@
 import numpy as np
 import pandas as pd
+
+raw_train_identity= pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Data for ai competitions\train_identity.csv')
+raw_test_identity = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Data for ai competitions\test_identity.csv')
+raw_train_transaction = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Data for ai competitions\train_transaction.csv')
+raw_test_transaction = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Data for ai competitions\test_transaction.csv')
+
+
